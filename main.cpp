4d94b17033c78b5161528947a63c03f4b9d13c74
kolusask <kolusask@github.com>
2020-03-11 11:53:36 +0100

    added code to print biconnected components

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 #include <set>
 #include <vector>
 
@@ -12,6 +13,7 @@ struct Node {
 };
 
 using Graph = std::vector<Node>;
+using Edge = std::pair<unsigned, unsigned>;
 
 Graph read_input() {
     unsigned nNodes, nEdges;
@@ -28,25 +30,48 @@ Graph read_input() {
 
 unsigned count = 0;
 
-void dfs(Graph& graph, const unsigned node, const unsigned parent, std::set<unsigned>& acc) {
+void dfs(Graph& graph, const unsigned node, const unsigned parent, std::set<unsigned>& ap, std::list<Edge>& st) {
     graph[node].in = ++count;
     for (unsigned ch : graph[node].adj) {
         if (graph[ch].in == UNDEF) {
             graph[node].children.insert(ch);
-            dfs(graph, ch, node, acc);
+            st.push_back(Edge(node, ch));
+            dfs(graph, ch, node, ap, st);
             if (graph[ch].low >= graph[node].in && (parent != INF || graph[node].children.size() > 1)) {
-                acc.insert(node);
+                ap.insert(node);
+                while (st.back().first != node || st.back().second != ch) {
+                    std::cout << st.back().first << "--" << st.back().second << "  ";
+                    st.pop_back();
+                }
+                std::cout << st.back().first << "--" << st.back().second;
+                st.pop_back();
+                std::cout << std::endl;
             }
             graph[node].low = std::min(graph[node].low, graph[ch].low);
-        } else if (graph[ch].in < graph[node].in && ch != parent)
+        } else if (graph[ch].in < graph[node].in && ch != parent) {
             graph[node].low = std::min(graph[node].low, graph[ch].in);
+            if (graph[ch].in < graph[node].in)
+                st.push_back(Edge(node, ch));
+        }
     }
 }
 
 std::set<unsigned> find_articulation_points(Graph& graph) {
-    std::set<unsigned> result;
-    dfs(graph, 0, INF, result);
-    return result;
+    std::set<unsigned> artPoints;
+        std::list<Edge> edgeStack;
+    for (unsigned i = 0; i < graph.size(); i++) {
+        if (graph[i].in == UNDEF)
+            dfs(graph, i, INF, artPoints, edgeStack);
+        bool j = false;
+        while (edgeStack.size() > 0) {
+            j = true;
+            std::cout << edgeStack.back().first << "--" << edgeStack.back().second << "  ";
+            edgeStack.pop_back();
+        }
+        if (j)
+            std::cout << std::endl;
+    }
+    return artPoints;
 }
 
 int main() {
@@ -90,4 +115,20 @@ int main() {
     6 7
     7 8
     9 8
+    ---
+    12 14
+    0 1
+    0 6
+    1 2
+    1 3
+    2 3
+    3 4
+    2 4
+    1 5
+    5 6
+    5 7
+    7 8
+    5 8
+    8 9
+    10 11
 */
